e34344d89ddba4db40759186bbd04b7de33625b6
Chris Cummins <chrisc.101@gmail.com>
2013-11-03 12:59:35 +0000

    tools/workflow: Add get_git_toplevel()
    Issue #28.

--- tools/workflow.sh
+++ tools/workflow.sh
@@ -14,17 +14,23 @@ fail_if_tree_not_clean() {
 	echo "clean?"
 }
 
+# Get git toplevel directory
+#    @return Path to git toplevel
+get_git_toplevel() {
+	git rev-parse --show-toplevel
+}
+
 # Create a new local work-in-progress branch
 #    $1 issue number
 new() {
 	fail_if_tree_not_clean
-	echo "new"
+	echo "new in `get_git_toplevel`"
 }
 
 # Close the current work-in-progress branch and rebase on master
 close() {
 	fail_if_tree_not_clean
-	echo "close"
+	echo "close in `get_git_toplevel`"
 }
 
 main() {
