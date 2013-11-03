a1310a2937a245495f051e98d8f33a24b274be38
Chris Cummins <chrisc.101@gmail.com>
2013-11-03 15:14:17 +0000

    Add prototype advanced search page
    Issue #38.

---
+++ www/advanced.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>pip-db Advanced Search</title>
+
+    <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+
+  <body>
+    <div class="container">
+
+    </div>
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+
+</html>
