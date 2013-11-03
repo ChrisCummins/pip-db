85e41286da49c354c35372ec4cc2bb9845c37292
Chris Cummins <chrisc.101@gmail.com>
2013-11-03 15:24:16 +0000

    advanced: Add placeholder text
    Issue #38.

--- www/advanced.php
+++ www/advanced.php
@@ -21,6 +21,35 @@
 
     <div class="container">
 
+      <div class="advanced-search">
+        <h4>Find proteins with...</h4>
+
+        <p>
+          The Closure Compiler is a tool for making JavaScript download and run
+          faster. It is a true compiler for JavaScript. Instead of compiling
+          from a source language to machine code, it compiles from JavaScript to
+          better JavaScript. It parses your JavaScript, analyzes it, removes
+          dead code and rewrites and minimizes what's left. It also checks
+          syntax, variable references, and types, and warns about common
+          JavaScript pitfalls.
+        </p>
+        <p>
+          HtmlCompressor is a small, fast and very easy to use Java library that
+          minifies given HTML or XML source by removing extra whitespaces,
+          comments and other unneeded characters without breaking the content
+          structure. As a result pages become smaller in size and load faster. A
+          command-line version of the compressor is also available.
+        </p>
+        <p>
+          The YUI Compressor is a JavaScript compressor which, in addition to
+          removing comments and white-spaces, obfuscates local variables using
+          the smallest possible variable name. This obfuscation is safe, even
+          when using constructs such as 'eval' or 'with' (although the
+          compression is not optimal is those cases) Compared to jsmin, the
+          average savings is around 20%.
+        </p>
+      </div>
+
     </div>
 
     <script src="//code.jquery.com/jquery.js"></script>
