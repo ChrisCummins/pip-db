eb3dd73b16a19ff52830c8f8eb2732039410e234
Chris Cummins <chrisc.101@gmail.com>
2013-11-04 22:27:18 +0000

    Add a login page
    Based on the bootstrap example:
    
    http://getbootstrap.com/examples/signin/

--- www/login.php
+++ www/login.php
@@ -4,16 +4,47 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>pip-db</title>
+    <title>pip-db Sign In</title>
 
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/styles.css"/>
   </head>
 
+  <style>
+    body {
+      background: #eee;
+      padding-top: 40px;
+      padding-bottom: 40px;
+    }
+  </style>
+
   <body>
 
       <!-- Main body of page -->
       <div class="container">
+
+        <form class="form-signin">
+          <h2 class="form-signin-heading">Login</h2>
+          <input type="text" class="form-control" placeholder="Email address" required autofocus>
+          <input type="password" class="form-control" placeholder="Password" required>
+          <label class="checkbox">
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+          <div style="display: table; width: 100%;">
+            <div style="display: table-cell; width: 50%; padding-right: 8px;">
+              <button class="btn btn-lg btn-primary btn-block"
+                      type="submit">
+                Register
+              </button>
+            </div>
+            <div style="display: table-cell; width: 50%; padding-left: 8px;">
+              <button class="btn btn-lg btn-success btn-block"
+                      type="submit">
+                Sign in
+              </button>
+            </div>
+          </div> <!-- /div -->
+        </form>
 
       </div> <!-- /.container -->
 
