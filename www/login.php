ece3e2c87ca5e0f2ef76e2d9b84ff2506781104e
Chris Cummins <chrisc.101@gmail.com>
2013-11-04 15:54:26 +0000

    Add empty login template

---
+++ www/login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>pip-db</title>
+
+    <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+
+  <body>
+
+      <!-- Main body of page -->
+      <div class="container">
+
+      </div> <!-- /.container -->
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+
+</html>
