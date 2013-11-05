96b7ea25c357d1bb5e6eef362f6e7d0de5fda2ad
Chris Cummins <chrisc.101@gmail.com>
2013-11-05 12:31:47 +0000

    Add an overview index page
    Issue #43.

---
+++ www/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>pip-db D1 Mockups</title>
+
+    <link rel="stylesheet" href="css/styles.css"/>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+  </head>
+
+  <style>
+    body {
+      padding-top: 40px;
+    }
+    .d1-picture {
+      max-width: 390px;
+    }
+    .d1-picture img {
+      width: 100%;
+    }
+    .d1-title {
+      margin-top: 10px;
+      margin-bottom: 60px;
+      text-align: center;
+    }
+  </style>
+
+  <body>
+
+    <!-- Content wrapper -->
+    <div id="wrap">
+      <div class="container d1-showcase">
+
+        <div class="jumbotron">
+          <h1>D1 Mockups</h1>
+          <p>This set of static web pages shows a mockup of the first iteration of design for pip-db.</p>
+          <p><a href="https://github.com/ChrisCummins/protein-db/tree/master/design/d1" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/search.php">
+                <img src="/img/homepage.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Homepage</h3>
+          </div> <!-- /.col-md-4 -->
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/advanced.php">
+                <img src="/img/advanced.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Advanced search</h3>
+          </div> <!-- /.col-md-4 -->
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/results.php">
+                <img src="/img/results.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Search results</h3>
+          </div> <!-- /.col-md-4 -->
+
+        </div> <!-- /.row -->
+
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/details.php">
+                <img src="/img/details.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Details page</h3>
+          </div> <!-- /.col-md-4 -->
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/login.php">
+                <img src="/img/login.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Login</h3>
+          </div> <!-- /.col-md-4 -->
+
+          <div class="col-md-4">
+            <div class="d1-picture">
+              <a href="/upload.php">
+                <img src="/img/upload.png">
+              </a>
+            </div>
+
+            <h3 class="d1-title">Add data</h3>
+          </div> <!-- /.col-md-4 -->
+
+        </div> <!-- /.row -->
+
+      </div> <!-- /container -->
+    </div> <!-- /.wrap -->
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
