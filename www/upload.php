4f095af02de96fc61f11aef0c4b99e16f7ba1ad5
Chris Cummins <chrisc.101@gmail.com>
2013-11-08 02:30:08 +0000

    Use get_footer() instead of static HTML
    Issue #63.

--- www/upload.php
+++ www/upload.php
@@ -221,24 +221,7 @@
       </div> <!-- /.container -->
     </div> <!-- /.wrap -->
 
-    <!-- Bottom of the page footer -->
-    <div id="footer">
-
-      <div class="left">
-        <div class="footer-inner">
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </div>
-      </div>
-
-      <div class="right">
-        <div class="footer-inner">
-          <a href="#">Terms</a>
-          <a href="#">Privacy</a>
-        </div>
-      </div>
-
-    </div> <!-- /.footer -->
+    <?php get_footer(); ?>
 
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
