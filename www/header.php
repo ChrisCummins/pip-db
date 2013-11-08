840276543935ea5201224b8e3873a7b6d9ba8a1c
Chris Cummins <chrisc.101@gmail.com>
2013-11-08 02:30:28 +0000

    header.php: Remove container in navbar

--- www/header.php
+++ www/header.php
@@ -5,7 +5,7 @@ function get_header( $inline_search = false, $value = null,
 
 	echo <<<EOF
       <!-- Fixed navbar -->
-      <div class="navbar navbar-fixed-top
+    <div class="navbar navbar-fixed-top
 EOF;
 
     if ($login_only == true) {
@@ -16,35 +16,33 @@ EOF;
 
     echo <<<EOF
 ">
-        <div class="container">
+        <div class="navbar-header">
 
-          <div class="navbar-header">
+          <!-- Collapse button -->
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
 
-            <!-- Collapse button -->
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
+          <!-- The logo -->
+          <a class="navbar-brand" href="search.php">pip-db</a>
 
-            <!-- The logo -->
-            <a class="navbar-brand" href="search.php">pip-db</a>
+        </div> <!-- /.navbar-header -->
 
-          </div> <!-- /.navbar-header -->
-
-          <div class="navbar-collapse collapse">
+        <div class="navbar-collapse collapse">
 EOF;
 
 	if ( true == $inline_search ) {
 		echo <<<EOF
-            <div class="col-sm-7 col-md-7 navbar-search">
+          <div class="col-sm-7 col-md-7 navbar-search">
 
-              <form class="navbar-form" role="search">
-                <div class="input-group">
+            <form class="navbar-form" role="search">
+              <div class="input-group">
 
-                  <!-- Search bar -->
-                  <input id="s" name="s" type="text" class="form-control"
-                         value="
+                <!-- Search bar -->
+                <input id="s" name="s" type="text" class="form-control"
+                       value="
 EOF;
 
 		$value = (string) $value;
@@ -56,40 +54,39 @@ EOF;
         	echo <<<EOF
 ">
 
-                  <!-- Search button -->
-                  <div class="input-group-btn">
-                    <a href="/search.php" class="btn btn-success" type="submit">
-                      Search
-                    </a>
-                  </div>
+                <!-- Search button -->
+                <div class="input-group-btn">
+                  <a href="/search.php" class="btn btn-success" type="submit">
+                    Search
+                  </a>
                 </div>
-              </form>
+              </div>
+            </form>
 
-            </div> <!-- /.col-md-7 -->
+          </div> <!-- /.col-md-7 -->
 
-            <!-- Advanced button -->
-            <ul class="nav navbar-nav">
-              <li>
-                <div class="navbar-button-group">
-                  <div class="navbar-button">
-                    <a href="/advanced.php" class="btn btn-primary">
-                      Advanced
-                    </a>
-                  </div>
+          <!-- Advanced button -->
+          <ul class="nav navbar-nav">
+            <li>
+              <div class="navbar-button-group">
+                <div class="navbar-button">
+                  <a href="/advanced.php" class="btn btn-primary">
+                    Advanced
+                  </a>
                 </div>
-              </li>
-            </ul> <!-- /.navnar-nav -->
+              </div>
+            </li>
+          </ul> <!-- /.navnar-nav -->
 EOF;
 	}
 
     echo <<<EOF
-            <!-- Login button -->
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="/login.php">Login</a></li>
-            </ul> <!-- /.navbar-right -->
+          <!-- Login button -->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/login.php">Login</a></li>
+          </ul> <!-- /.navbar-right -->
 
-          </div><!--/.nav-collapse -->
-        </div> <!-- /.container -->
+        </div><!--/.nav-collapse -->
       </div> <!-- /.navbar -->
 EOF;
 
