581851c3ec80fbaeb7cc0da1010d7dabd3efc3f0
Chris Cummins <chrisc.101@gmail.com>
2013-11-08 15:44:32 +0000

    tools/sloccount: Add get_project_root() function
    Issue #53.

--- tools/sloccount.sh
+++ tools/sloccount.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Lookup the root directory for the project. If unable to locate root, exit
+# script.
+#
+#     @return The absolute path to the project root directory
+get_project_root() {
+	local root=""
+
+	while [[ "$(pwd)" != "/" ]]; do
+		if test -f configure.ac; then
+			echo "$(pwd)"
+			return
+		fi
+		cd ..
+	done
+
+	echo "fatal: Unable to locate project base directory." >&2
+	exit 3
+}
+
 main() {
 	echo "$0"
 }
