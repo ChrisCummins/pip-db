c4ef4a99f4ddb1ed0b962d81f263314527ec20c6
Chris Cummins <chrisc.101@gmail.com>
2013-11-09 20:08:44 +0000

    Revert "Clean up test files"
    This reverts commit c64389491c839aa12e722025d7b9e663489d4800.

---
+++ www/template.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once( './lib/Twig/Autoloader.php' );
+
+Twig_Autoloader::register();
+
+$twig_loader = new Twig_Loader_Filesystem( './html' );
+$twig_args = array();
+
+$twig = new Twig_Environment( $twig_loader, $twig_args );
+
+function render_template( $template_name, $template_args = array() ) {
+	global $twig;
+
+	$template_extension = '.html';
+	$template_file = $template_name . $template_extension;
+
+	$template = $twig->loadTemplate( $template_file );
+
+	$template->display( $template_args );
+}
