060604d98387a72f9ca351f56a744958f95d6e10
Chris Cummins <chrisc.101@gmail.com>
2013-11-13 22:06:14 +0000

    www: Rename 'functions.php' to 'init.php'
    Issue #91.

--- www/results.php
+++ www/results.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once( 'lib/functions.php' );
+require_once( 'lib/init.php' );
 
 $content = array(
 	/*
--- www/search.php
+++ www/search.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once( 'lib/functions.php' );
+require_once( 'lib/init.php' );
 
 render_template( 'search' );
