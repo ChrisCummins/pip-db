31f42f9532a31dbc25664373e8f303f42978a953
Chris Cummins <chrisc.101@gmail.com>
2013-11-13 02:06:10 +0000

    autogen: Add autoconf_min and automake_min checks
    Based on Emacs' autogen script, but with improved regex for
    AM_INIT_AUTOMAKE which allows for extra options.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+## Tools we need:
+## Note that we respect the values of AUTOCONF etc, like autoreconf does.
+progs="autoconf automake"
+
+## Minimum versions we need:
+autoconf_min=$(sed -n 's/^ *AC_PREREQ(\([0-9\.]*\)).*/\1/p' configure.ac)
+
+automake_min=$(sed -n 's/^ *AM_INIT_AUTOMAKE( *\[\? *\([0-9\.]*\).*/\1/p' configure.ac)
+
+test -n "$autoconf_min" || exit 127
+test -n "$automake_min" || exit 127
+
 autoreconf --install
 
 test -n "$srcdir" || srcdir=`dirname "$0"`
