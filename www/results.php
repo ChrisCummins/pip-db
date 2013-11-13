50d8899f9166f7f7f42881a061728a6a42ac6c8e
Chris Cummins <chrisc.101@gmail.com>
2013-11-13 21:20:26 +0000

    results.php: Add a results array
    Issue #74.

--- www/results.php
+++ www/results.php
@@ -2,6 +2,225 @@
 
 require_once( 'lib/functions.php' );
 
-$content = array();
+$content = array(
+	/*
+	 * (optional)
+	 * An array of search results. Each result element is an array of
+	 * properties, with the first element being the href to that
+	 * corresponding record's details page.
+	 */
+	"results" => array(
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			),
+		array(
+			"details.php",
+			"Alkaline phosphatese",
+			"Human",
+			"Placenta",
+			"4.6"
+			)
+		)
+	);
 
 render_template( 'results', $content );
