bfd7c468f838378812d1d939be35c9f212b6a797
Chris Cummins <chrisc.101@gmail.com>
2013-11-13 14:08:09 +0000

    details.php: Add $content.names property
    Issue #74.

--- www/details.php
+++ www/details.php
@@ -2,6 +2,14 @@
 
 require_once( 'lib/functions.php' );
 
-$content = array();
+$content = array(
+	/* The names of the protein. Each protien must have at least one name,
+	 * which is used to set the page title. Additional names after the first
+	 * element will be listed as 'Alternative names'. */
+	"names" => array(
+		"Alkaline phosphatese",
+		"Phosphatese alkaline",
+		)
+	);
 
 render_template( 'details', $content );
