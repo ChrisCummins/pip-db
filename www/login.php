0a71c36b0d85d723340458c96f92ff9d390b3fcd
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 21:12:27 +0000

    login.php: Redirect to homepage if no referrer is set
    Issue #97.

--- www/login.php
+++ www/login.php
@@ -2,11 +2,17 @@
 
 require_once( $_SERVER['PHP_ROOT'] . 'init.php' );
 
+/* Set the referring page, if applicable, else redirect home */
+if ( isset( $_SERVER['HTTP_REFERER'] ) )
+	$referer = $_SERVER['HTTP_REFERER'];
+else
+	$referer = '/search.php';
+
 $content = array(
 	/*
 	 * The referring page for this login request.
 	 */
-	"referer" => $_SERVER['HTTP_REFERER']
+	"referer" => $referer
 	);
 
 pip_render_template( 'login', $content );
