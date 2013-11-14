a513738deb4c26b67f616ea8aafaeeaa935a54b0
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 21:47:29 +0000

    login.php: Use pip_history_get_referer()

--- www/login.php
+++ www/login.php
@@ -2,17 +2,11 @@
 
 require_once( $_SERVER['PHP_ROOT'] . 'init.php' );
 
-/* Set the referring page, if applicable, else redirect home */
-if ( isset( $_SERVER['HTTP_REFERER'] ) )
-	$referer = $_SERVER['HTTP_REFERER'];
-else
-	$referer = '/search.php';
-
 $content = array(
 	/*
 	 * The referring page for this login request.
 	 */
-	"referer" => $referer
+	"referer" => pip_history_get_referer()
 	);
 
 pip_render_template( 'login', $content );
