3e172810162aec84e4c825362ea8105ca79b7b7b
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 21:47:12 +0000

    lib: Add a history.php API

---
+++ www/lib/history.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Returns the URL of the referring page. If there is none, then default to the
+ * homepage.
+ */
+function pip_history_get_referer() {
+	if ( isset( $_SERVER['HTTP_REFERER'] ) )
+		return $_SERVER['HTTP_REFERER'];
+	else
+		return '/search.php';
+}
