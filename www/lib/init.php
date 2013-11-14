ef551c5160ecb070aa43e2f36c34750a057ff432
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 10:20:19 +0000

    Add a post.php API

--- www/lib/init.php
+++ www/lib/init.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once( 'post.php' );
 require_once( 'session.php' );
 
 require_once( 'template.php' );
---
+++ www/lib/post.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * The names of valid POST variables. All accessing of POST variables should be
+ * done indirectly using the pip_{get,set}_post_var() API and using the values
+ * found in this class.
+ */
+abstract class PostVariables
+{
+	const Username = "user";
+	const Password = "pass";
+}
+
+/*
+ * Returns wheteher a particular POST variable is set or not.
+ */
+function pip_post_var_isset( $var ) {
+	return isset( $_POST[$var] );
+}
+
+/*
+ * Return a particular POST variable if defined, else an empty string.
+ */
+function pip_get_post_var( $var ) {
+	if ( pip_post_var_isset( $_POST[$var] ) )
+		return $_POST[$var];
+	else
+		return "";
+}
