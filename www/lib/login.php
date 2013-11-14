da1c4041dfd05f5c14b76d2283c40d3f0f3af907
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 21:37:58 +0000

    login.php: Add registration attempt

--- www/lib/login.php
+++ www/lib/login.php
@@ -1,6 +1,14 @@
 <?php
 
 /*
+ * Legal values for the POST['action'] variable.
+ */
+abstract class PostActionValues {
+	const Login = "login"; /* Attempting to login */
+	const Register = "register"; /* Attempting to register a new account */
+}
+
+/*
  * Returns whether the user is currently logged in or not.
  */
 function pip_is_logged_in() {
@@ -53,10 +61,29 @@ function pip_login( $username, $password ) {
 }
 
 /*
+ * Registers a new account.
+ */
+function pip_add_account( $username, $password ) {
+	/*
+	 * TODO: Actually implement a proper user backend. For now, we just
+	 * assume that whatever details were provided were successful.
+	 */
+}
+
+/*
  * Returns whether we're currently attempting a page login.
  */
 function pip_attempting_login() {
-	return pip_post_var_isset( PostVariables::Username );
+	return PostActionValues::Login ==
+		pip_get_post_var( PostVariables::Action );
+}
+
+/*
+ * Returns whether the user is currently attempting to register a new account.
+ */
+function pip_attempting_register() {
+	return PostActionValues::Register ==
+		pip_get_post_var( PostVariables::Action );
 }
 
 /*
@@ -82,6 +109,44 @@ function pip_do_login_attempt() {
 		pip_failed_login();
 }
 
+/*
+ * Called when a registration attempt is unsuccessful.
+ */
+function pip_failed_register() {
+	/*
+	 * TODO: Provide visual feedback to the user that the attempt was
+	 * unsuccessful.
+	 */
+}
+
+/*
+ * Returns whether a specific user name exists.
+ */
+function pip_user_exists( $username ) {
+	/*
+	 * TODO: Actually implement a proper user backend. For now, we just
+	 * assume that no users exist.
+	 */
+	return false;
+}
+
+/*
+ * Attempt to register a new user.
+ */
+function pip_do_register_attempt() {
+	$username = pip_get_post_var( PostVariables::Username );
+	$password = pip_get_post_var( PostVariables::Password );
+
+	if ( !pip_user_exists( $username ) ) {
+		pip_add_account( $username, $password );
+		pip_login( $username, $password );
+	} else
+		pip_failed_register();
+
+}
+
 /* Handle login attempt */
 if ( pip_attempting_login() )
 	pip_do_login_attempt();
+else if ( pip_attempting_register() )
+	pip_do_register_attempt();
