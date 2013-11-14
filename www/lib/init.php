d20b52a9a093619217c21d52c018543b0d06cac0
Chris Cummins <chrisc.101@gmail.com>
2013-11-14 21:46:45 +0000

    init.php: Minor refactor

--- www/lib/init.php
+++ www/lib/init.php
@@ -3,6 +3,6 @@
 require_once( $_SERVER['PHP_ROOT'] . 'post.php' );
 require_once( $_SERVER['PHP_ROOT'] . 'session.php' );
 
-require_once( $_SERVER['PHP_ROOT'] . 'login.php' );
-
 require_once( $_SERVER['PHP_ROOT'] . 'template.php' );
+
+require_once( $_SERVER['PHP_ROOT'] . 'login.php' );
