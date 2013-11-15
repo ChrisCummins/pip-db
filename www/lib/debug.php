620960b3680abdf29abdddcfc1690f0970e41458
Chris Cummins <chrisc.101@gmail.com>
2013-11-15 01:08:53 +0000

    www: Add optional debugging

---
+++ www/lib/debug.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( isset($_SERVER['DEBUG']) ) {
+
+	/* Display errors */
+	ini_set( 'display_errors', 'On' );
+
+}
--- www/lib/init.php
+++ www/lib/init.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once( $_SERVER['PHP_ROOT'] . 'debug.php' );
+
 require_once( $_SERVER['PHP_ROOT'] . 'superglobals.php' );
 require_once( $_SERVER['PHP_ROOT'] . 'post.php' );
 require_once( $_SERVER['PHP_ROOT'] . 'session.php' );
