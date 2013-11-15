7393beaf0b9a41506ff66454120fa61d580dcaa0
Chris Cummins <chrisc.101@gmail.com>
2013-11-15 01:46:08 +0000

    debug.php: Add shutdown and error handlers

--- www/lib/debug.php
+++ www/lib/debug.php
@@ -1,11 +1,49 @@
 <?php
 
+/*
+ * Error handler.
+ */
+function pip_error_handler( $num, $str, $file, $line, $context = null )
+{
+	pip_exception_handler( new ErrorException( $str, 0, $num, $file, $line ) );
+}
+
+/*
+ * Exception handler.
+ */
+function pip_exception_handler( Exception $e ) {
+	print "<div style='text-align: center;'>";
+	print "<h2 style='color: rgb(190, 50, 50);'>Exception Occured:</h2>";
+	print "<table style='width: 800px; display: inline-block;'>";
+	print "<tr style='background-color:rgb(230,230,230);'><th style='width: 80px;'>Type</th><td>" . get_class( $e ) . "</td></tr>";
+	print "<tr style='background-color:rgb(240,240,240);'><th>Message</th><td>{$e->getMessage()}</td></tr>";
+	print "<tr style='background-color:rgb(230,230,230);'><th>File</th><td>{$e->getFile()}</td></tr>";
+	print "<tr style='background-color:rgb(240,240,240);'><th>Line</th><td>{$e->getLine()}</td></tr>";
+	print "</table></div>";
+	exit();
+}
+
+/**
+ * Shutdown handler.
+ */
+function shutdown_handler() {
+	$e = error_get_last();
+
+	if ( $e['type'] == E_ERROR )
+		pip_error_handler( $e['type'], $e['message'], $e['file'], $e['line'] );
+}
+
 if ( isset($_SERVER['DEBUG']) ) {
 
 	/* Display errors */
-	ini_set( 'display_errors', 'On' );
+	ini_set( 'display_errors', 'Off' );
 
 	/* Set a high error-reporting level */
 	error_reporting( E_ALL | E_STRICT );
+
+	/* Register our custom error handlers */
+	set_error_handler( 'pip_error_handler' );
+	set_exception_handler( 'pip_exception_handler' );
+	register_shutdown_function( 'shutdown_handler' );
 
 }
