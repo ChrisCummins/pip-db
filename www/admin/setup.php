c73d380ec6d73afc3cf074be28fbf12f0faeb5cb
Chris Cummins <chrisc.101@gmail.com>
2013-11-19 00:04:04 +0000

    setup.{php,html}: Create tables
    Issue #69.

--- www/admin/setup.php
+++ www/admin/setup.php
@@ -2,6 +2,19 @@
 
 require_once( $_SERVER['PHP_ROOT'] . 'init.php' );
 
-$content = array();
+$content = array( "tables" => array() );
+
+foreach ( PipDatabase::schema() as $table => $description ) {
+
+	/* Add table to template content. */
+	array_push( $content["tables"], array( "name" => $table,
+					       "schema" => $description ) );
+
+	/* Create database table. */
+	if ( !pip_db_table_create( $table, $description ) ) {
+		throw new PipDbException('Failed to create table: \'' . $table + '\'!' );
+	}
+
+}
 
 pip_render_template( 'setup', $content );
