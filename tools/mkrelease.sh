50e37ab3c09f4813b4f6a5be2fdb32e7a55ef8e3
Chris Cummins <chrisc.101@gmail.com>
2013-11-19 21:32:24 +0000

    tools/mkrelease: Add a get_project_root() function
    Issue #116.

--- tools/mkrelease.sh
+++ tools/mkrelease.sh
@@ -5,6 +5,25 @@ usage() {
 	echo "Usage: $0 <version>"
 }
 
+# Lookup the root directory for the project. If unable to locate root, exit
+# script.
+#
+#     @return The absolute path to the project root directory
+get_project_root() {
+	local root=""
+
+	while [[ "$(pwd)" != "/" ]]; do
+		if test -f configure.ac; then
+			echo "$(pwd)"
+			return
+		fi
+		cd ..
+	done
+
+	echo "fatal: Unable to locate project base directory." >&2
+	exit 3
+}
+
 main() {
 	# Set debugging output if DEBUG=1
 	test -n "$DEBUG" && {
