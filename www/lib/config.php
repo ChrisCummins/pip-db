2e3617ad87da8e7f9fe5dd264693f1882ebf5abe
Chris Cummins <chrisc.101@gmail.com>
2013-11-19 00:03:13 +0000

    config.php: Add a database schema property
    Issue #69.

--- www/lib/config.php
+++ www/lib/config.php
@@ -11,4 +11,21 @@ abstract class PipDatabase {
 	const Name = 'pipdb';
 	const Engine = 'INNODB';
 
+	static function schema() {
+		return array(
+			'users' =>
+"user_id bigint(20) UNSIGNED NOT NULL auto_increment,
+email varchar(200) NOT NULL DEFAULT '',
+pass varchar(255) NOT NULL DEFAULT '',
+user_type_id bigint(20) UNSIGNED NOT NULL,
+PRIMARY KEY (user_id),
+INDEX (user_type_id),
+FOREIGN KEY (user_type_id) REFERENCES user_types(user_type_id)",
+			'user_types' =>
+"user_type_id bigint(20) UNSIGNED NOT NULL auto_increment,
+type_name varchar(255) NOT NULL DEFAULT '',
+PRIMARY KEY (user_type_id)"
+			);
+	}
+
 }
