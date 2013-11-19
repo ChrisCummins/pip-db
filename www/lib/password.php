3535c6704eac8e271265933ec6ae54d468246d64
Chris Cummins <chrisc.101@gmail.com>
2013-11-19 00:23:03 +0000

    lib: Add a password API
    Issue #50.

---
+++ www/lib/password.php
@@ -0,0 +1,12 @@
+<?php
+
+function pip_password_get_hash ( $password, $rounds = 7 ) {
+
+	$salt = "";
+	$salt_chars = array_merge(range('A','Z'), range('a','z'), range(0,9));
+
+	for($i=0; $i < 22; $i++)
+		$salt .= $salt_chars[array_rand($salt_chars)];
+
+	return crypt($password, sprintf('$2a$%02d$', $rounds) . $salt);
+}
