9590159be9fd0007742643faf6839cff4a7614ed
Chris Cummins <chrisc.101@gmail.com>
2013-11-20 18:10:47 +0000

    r.php: Minor style fixes

--- www/r.php
+++ www/r.php
@@ -15,34 +15,34 @@ $content = array(
 	 * (optional)
 	 * The href to download page.
 	 */
-	 "download" => "http://127.0.0.1",
+	"download" => "http://127.0.0.1",
 	/*
 	 * The names of the protein. Each protien must have at least one name,
 	 * which is used to set the page title. Additional names after the first
 	 * element will be listed as 'Alternative names'.
 	 */
-	 "names" => $record->get_names(),
+	"names" => $record->get_names(),
 	/*
 	 * The record properties. These are in <key> <value> pairs where the key
 	 * is the table entry name.
 	 */
-	 "properties" => $record->get_properties(),
+	"properties" => $record->get_properties(),
 	/*
 	 * (optional)
 	 * The record meta-data.
 	 */
-	 "meta" => $record->get_meta(),
+	"meta" => $record->get_meta(),
 	/*
 	 * (optional)
 	 * The external links properties. These are in <name, href> pairs and
 	 * represent individual external reference buttons.
 	 */
-	 "links" => $record->get_links(),
+	"links" => $record->get_links(),
 	/*
 	 * (optional)
 	 * The "Reference this Page" section.
 	 */
-	 "ref" => $record->get_ref()
+	"ref" => $record->get_ref()
 	);
 
 pip_render_template( 'details', $content );
