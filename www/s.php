f370bf78a3f0eb04f93b4a7a8c4b3d4c9bcc344e
Chris Cummins <chrisc.101@gmail.com>
2013-11-21 19:45:41 +0000

    s.php: Enable pagination
    Issue #141.

--- www/s.php
+++ www/s.php
@@ -28,7 +28,21 @@ $base_url .= $_SERVER['SERVER_NAME'];
 $base_url .= '/s?';
 $base_url .= GetVariables::Query . '=' . urlencode( $search_text );
 
-$num_of_pages = ceil($results_count / Pip_Search::ResultsPerPage );
+/* Calculate page numbers and whatnot */
+$num_of_pages = ceil( $results_count / Pip_Search::ResultsPerPage );
+$starting_at = pip_get_isset( GetVariables::StartAt ) ? pip_get( GetVariables::StartAt ) : 0;
+$ending_at = $starting_at + Pip_Search::ResultsPerPage;
+$current_page = $starting_at / Pip_Search::ResultsPerPage + 1;
+
+/* Generate pagination hrefs */
+$pages = array();
+for ( $i = 0; $i < $num_of_pages; $i++ ) {
+	$url = $base_url;
+	if ($i)
+		$url .= '&start=' . $i * Pip_search::ResultsPerPage;
+
+	array_push( $pages, $url );
+}
 
 $content = array(
 	/*
@@ -43,11 +57,22 @@ $content = array(
 	/*
 	 * An array of results. This can be empty if no results were found.
 	 */
-	"results" => $results,
+	"results" => array_slice( $results, $starting_at, $ending_at ),
 	/*
 	 * The number of results returned.
 	 */
 	"results_count" => $results_count,
+	/*
+	 * (optional)
+	 * Href to pagination links, if we returned more than one page.
+	 */
+	"pages" => $pages,
+	/*
+	 * (optional)
+	 * If the results span multiple pages, set this to the page number
+	 * currently being displayed.
+	 */
+	"current_page" => $current_page
 	);
 
 pip_render_template( 'results', $content );
