824756392cb148b014bac52fff109bb8a4854918
Chris Cummins <chrisc.101@gmail.com>
2013-11-23 00:56:49 +0000

    404.{php,html}: Render message from template contents

--- www/404.php
+++ www/404.php
@@ -2,5 +2,11 @@
 
 require_once( $_SERVER['PHP_ROOT'] . 'init.php' );
 
+$content = array(
+	"error" => array(
+		'message' => "Sorry, I couldn't find the page you're after."
+		)
+	);
+
 $template = new Pip_Template( '404' );
-$template->render( );
+$template->render( $content );
