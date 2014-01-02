e7da24447ddcce1a957ea952ad19606bcafe5885
Chris Cummins <chrisc.101@gmail.com>
2014-01-02 14:28:43 +0000

    Add a script to create a local postgres instance
    The mkdb.sh script generates a local postgres database cluster and
    creates the required pip-db database.

---
+++ scripts/mkdb.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Setup a local instance of the postgres db.
+#
+topsrcdir=pip-db # Top level directory
+dir=pg           # Database directory
+dbname=pip-db    # Database name
+
+# Look up the top source directory. Do this by traversing up the directory tree
+# from the current directory. If we haven't found the top directory by the time
+# we reach root (/), fail.
+get_source_directory() {
+    local root=""
+
+    while [[ "$(pwd)" != "/" ]]; do
+        if [[ "${PWD##*/}" = "$topsrcdir" ]]; then
+            echo "$(pwd)"
+            return
+        fi
+        cd ..
+    done
+
+    echo "fatal: Unable to locate source directory." >&2
+
+    # If we can't find a real path to change to, then return something bogus
+    # so that cd will fail and the script will terminate.
+    echo "/not/a/real/path"
+}
+
+set -e
+
+cd $(get_source_directory) 2>/dev/null
+
+echo "Creating PostreSQL database cluster in '$dir'..."
+
+test -d $dir && { echo "Directory '%dir' already exists!"; exit 5; }
+
+initdb $dir
+
+echo "Starting PostgreSQL server..."
+postgres -D $dir &
+
+echo "Creating PostgreSQL database '$dbname'..."
+createdb $dbname
