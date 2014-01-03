bf5162889be49d24186a3cc369989b160a4f32b1
Chris Cummins <chrisc.101@gmail.com>
2014-01-03 21:35:59 +0000

    models/search: Build query trees

--- src/pip_db/models/search.clj
+++ src/pip_db/models/search.clj
@@ -1,10 +1,23 @@
 (ns pip-db.models.search
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [clojure.string :as str]))
+
+(defn condition [field value]
+  (str "LOWER(" field ") LIKE LOWER('%" value "%')"))
+
+(defn composite [join & conditions]
+  (str "(" (str/join (str " " join " ") conditions) ")"))
+
+(defn build-query [params]
+  (str "SELECT id,name,source,organ,pi FROM records WHERE "
+       (composite "AND"
+                  (composite "OR"
+                             (condition "name" (get params "q"))
+                             (condition "alt_name" (get params "q"))))))
 
 (defn query [params]
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
-      [(str "SELECT id,name,source,organ,pi FROM records WHERE "
-            "LOWER(name) LIKE LOWER('%" (get params "q") "%')")]
+      [(build-query params)]
       (if (= results nil)
         nil (doall results)))))
