9ee638c38bd0a5e33b2707309e4dc22edfd9963c
Chris Cummins <chrisc.101@gmail.com>
2014-01-03 21:59:41 +0000

    Add advanced search implementation

--- src/pip_db/controllers/search.clj
+++ src/pip_db/controllers/search.clj
@@ -1,11 +1,13 @@
 (ns pip-db.controllers.search
   (:use [compojure.core :only (defroutes GET)])
   (:require [pip-db.models.search :as model]
+            [pip-db.views.advanced :as advanced]
             [pip-db.views.search :as view]))
 
 (defn search [query results]
   (view/search query results))
 
 (defroutes routes
+  (GET "/advanced" [] (advanced/advanced))
   (GET "/s" {params :params}
        (search (get params "q") (model/query params))))
