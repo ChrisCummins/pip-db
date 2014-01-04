4ad959f8cf550ea2f5856fed8729e08d7e1c451d
Chris Cummins <chrisc.101@gmail.com>
2014-01-04 02:33:03 +0000

    query: Strip empty conditions using regexp

--- src/pip_db/query.clj
+++ src/pip_db/query.clj
@@ -1,8 +1,14 @@
 (ns pip-db.query
   (:require [clojure.string :as str]))
 
+;; The regular expression literal to match empty conditions. There can
+;; be two types of empty conditions: empty text conditions and empty
+;; compound conditions, which manifest themselves as "" and "()",
+;; respectively.
+(def re-empty-condition #" *\(? *\)?\ *")
+
 (defn strip-conditions [conditions]
-  (filter (fn [condition] (not (= "()" condition))) conditions))
+  (filter (fn [x] (not (re-matches re-empty-condition x))) conditions))
 
 (defn compound-conditional [join conditions]
   (str "(" (str/join join (strip-conditions conditions)) ")"))
