24a0e401755934930198679c84383c7bbd52bca2
Chris Cummins <chrisc.101@gmail.com>
2014-01-04 22:29:29 +0000

    Implement pagination
    This emulates the pagination behaviour of the PHP implementation.

--- src/pip_db/controllers/search.clj
+++ src/pip_db/controllers/search.clj
@@ -1,10 +1,12 @@
 (ns pip-db.controllers.search
   (:use [compojure.core :only (defroutes GET)])
-  (:require [pip-db.models.search :as model]
+  (:require [clojure.contrib.math :as math]
+            [pip-db.models.search :as model]
             [pip-db.views.advanced :as advanced]
             [pip-db.views.search :as view]))
 
 (def results-per-page 20)
+(def max-page-links 10)
 
 (defn search [query results]
   (view/search query results))
@@ -26,11 +28,23 @@
 (defn paginate [data start]
   (if (nil? start)
     (paginate data 0)
-    (assoc data :results (visible-results (data :results)
-                                          (data :results-count)
-                                          start (+ start results-per-page)))))
+    (let [results         (data :results)
+          results-count   (data :results-count)
+          start-at        (start-at start results-count)
+          end-at          (end-at (+ start-at results-per-page) results-count)
+          pages-count     (math/ceil (/ results-count results-per-page))
+          current-page    (+ (/ start-at results-per-page) 1)
+          start-page      (max 1 (- current-page (/ max-page-links 2)))
+          end-page        (min (+ pages-count 1) (+ start-page max-page-links))]
+      (assoc data
+        :results          (visible-results results results-count
+                                           start-at end-at)
+        :current-page     current-page
+        :results-per-page results-per-page
+        :pages-count      pages-count
+        :pages            (range start-page end-page)))))
 
-(defn start-at [params]
+(defn start-param [params]
   (try (Integer/parseInt
         (get params "start"))
        (catch Exception e 0)))
@@ -39,4 +53,4 @@
   (GET "/advanced" [] (advanced/advanced))
   (GET "/s" {params :params}
        (search (get params "q") (paginate (model/search params)
-                                          (start-at params)))))
+                                          (start-param params)))))
--- src/pip_db/views/search.clj
+++ src/pip_db/views/search.clj
@@ -18,6 +18,28 @@
        [:td (record :organ)]
        [:td (record :pi)]])]])
 
+(defn page-links [current-page pages pages-count]
+  (list
+   (if (> (first pages) 1)
+     [:a.page-ref.btn.btn-success {:data-page 1} "&laquo;"])
+   (for [page pages]
+     [:a.page-ref.btn.btn-success
+      (if (= page current-page)
+        {:data-page page :class "disabled"}
+        {:data-page page}) page])
+   (if (< (last pages) pages-count)
+     [:a.page-ref.btn.btn-success {:data-page pages-count} "&raquo;"])))
+
+(defn pagination-links [current-page pages results-per-page pages-count]
+  [:div.row {:style "margin-bottom: 20px;"}
+   [:div.col-lg-12
+    [:div {:style "text-align: center;"}
+     [:div#pagination.btn-group {:style "margin: 0 auto;"
+                                 :data-results-per-page results-per-page
+                                 :data-pages-count pages-count}
+      (page-links current-page pages pages-count)]]]])
+
+
 (defn search [query data]
   (page {:title query
          :navbar {:search true :search-text query}
@@ -26,6 +48,10 @@
                    :download "/"}
          :body [:div.sresults
                 (if (> (data :results-count) 0)
-                  (tablify-results (data :results))
+                  (list
+                   (tablify-results (data :results))
+                   (pagination-links (data :current-page) (data :pages)
+                                     (data :results-per-page)
+                                     (data :pages-count)))
                   [:p.lead "No results found."])]
          :javascript (inline-js "/js/search.js")}))
