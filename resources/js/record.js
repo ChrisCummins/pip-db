2fca0f39dadfd921b316b36e3fffd9fa002952a3
Chris Cummins <chrisc.101@gmail.com>
2014-01-05 02:27:31 +0000

    js/record: Implement client side property lookup
    This follow up to HEAD~
    
        0bd8ebb9ccc946b15829bba7997c18b89f451df6
        Author: Chris Cummins <chrisc.101@gmail.com>
        Date:   Sun Jan 5 02:24:30 2014 +0000
    
            views/record: Set data-key properties in table
    
    Which implements a linear time lookup function for record properties.

--- resources/js/record.js
+++ resources/js/record.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
+
+  /*
+   * Lookup a property by key. The key must be a string which matches
+   * a data-key tag. The property's value is returned as a string. Example:
+   *
+   *   get_property("name")
+   *     -> "Alkaline phosphatase"
+   *
+   * If a property matching the key is not found, undefined is
+   * returned. This is a list lookup, with linear O(n) time
+   * performance.
+   */
+  var get_property = function(key) {
+    var value = undefined;
+
+    $('#properties .property').each(function (index) {
+      var property = $(this).attr('data-key');
+
+      console.log(property + ', KEY: ' + key);
+
+      if (key === property) {
+        value = $(this).children('.value').text();
+        return false;           // Break loop
+      }
+    });
+
+    return value;
+  };
+
   /*
    * REFERENCE THIS PAGE:
    */
-  $('#reference').html('pip-db (2014). <i>' +
-                       window.document.title.replace(/pip-db /, '') +
+  $('#reference').html('pip-db (2014). <i>' + get_property("name") +
                        '</i>, Protein Isoelectric point Database. ' +
                        'Available from: &lt;<span class=\"url\">' +
                        window.location + '</span>&gt;<br/>[Cited ' +
