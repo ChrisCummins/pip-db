6d9e7b35e3baaeb0d4b146e1ca7f03eb808efdb7
Chris Cummins <chrisc.101@gmail.com>
2014-01-12 20:56:51 +0000

    Apply jslint recommendations to JavaScript
    Issue #231.

--- resources/js/advanced.inline.js
+++ resources/js/advanced.inline.js
@@ -1,37 +1,35 @@
-$(function() {
+$(function () {
+    'use strict';
 
-  function hashDiff(h1, h2){
-    var diff = {};
+    var hashDiff = function (h1, h2) {
+        var diff = {}, k;
 
-    for (k in h2) {
-      if (h1[k] !== h2[k])
-        diff[k] = h2[k];
-    }
+        for (k in h2) {
+            if (h1[k] !== h2[k]) { diff[k] = h2[k]; }
+        }
 
-    return diff;
-  }
+        return diff;
+    },
+        convertSerializedArrayToHash = function (a) {
+            var hash = {};
 
-  function convertSerializedArrayToHash(a){
-    var hash = {};
+            a.forEach(function (element) {
+                hash[element.name] = element.value;
+            });
 
-    a.forEach(function(element) {
-      hash[element.name] = element.value;
-    })
+            return hash;
+        },
+        $form = $('#as'),
+        startItems = convertSerializedArrayToHash($form.serializeArray());
 
-    return hash;
-  }
+    $form.submit(function (event) {
 
-  var $form = $('#as');
-  var startItems = convertSerializedArrayToHash($form.serializeArray());
+        event.preventDefault();
 
-  $form.submit(function(event) {
+        var currentItems = convertSerializedArrayToHash($form.serializeArray()),
+            itemsToSubmit = hashDiff(startItems, currentItems);
 
-    event.preventDefault();
-
-    var currentItems = convertSerializedArrayToHash($form.serializeArray());
-    var itemsToSubmit = hashDiff(startItems, currentItems);
-
-    window.location = $form.attr('action') + '?' + $.param(itemsToSubmit);
-  });
+        window.location = $form.attr('action') + '?' + $.param(itemsToSubmit);
+    });
 
 });
--- resources/js/record.inline.js
+++ resources/js/record.inline.js
@@ -1,48 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    'use strict';
 
-  /*
-   * Lookup a property by key. The key must be a string which matches
-   * a data-key tag. The property's value is returned as a string. Example:
-   *
-   *   get_property("name")
-   *     -> "Alkaline phosphatase"
-   *
-   * If a property matching the key is not found, undefined is
-   * returned. This is a list lookup, with linear O(n) time
-   * performance.
-   */
-  var get_property = function(key) {
-    var value = undefined;
+    /*
+     * Lookup a property by key. The key must be a string which matches
+     * a data-key tag. The property's value is returned as a string. Example:
+     *
+     *   get_property("name")
+     *     -> "Alkaline phosphatase"
+     *
+     * If a property matching the key is not found, undefined is
+     * returned. This is a list lookup, with linear O(n) time
+     * performance.
+     */
+    var get_property = function (key) {
+        var value;
 
-    $('#properties .property').each(function (index) {
-      var property = $(this).attr('data-key');
+        $('#properties .property').each(function (index) {
+            var property = $(this).attr('data-key');
 
-      console.log(property + ', KEY: ' + key);
+            console.log(property + ', KEY: ' + key);
 
-      if (key === property) {
-        value = $(this).children('.value').text();
-        return false;           // Break loop
-      }
-    });
+            if (key === property) {
+                value = $(this).children('.value').text();
+                return false;           // Break loop
+            }
+        });
 
-    return value;
-  };
+        return value;
+    };
 
-  /*
-   * REFERENCE THIS PAGE:
-   */
-  $('#reference').html('pip-db (2014). <i>' + get_property("name") +
-                       '</i>, Protein Isoelectric point Database. ' +
-                       'Available from: &lt;<a href="' + window.location +
-                       '"" class=\"url\">' + window.location +
-                       '</a>&gt;<br/>[Cited ' +
-                       moment().format('Do MMMM, YYYY') + '].');
+    /*
+     * REFERENCE THIS PAGE:
+     */
+    $('#reference').html('pip-db (2014). <i>' + get_property("name") +
+                         '</i>, Protein Isoelectric point Database. ' +
+                         'Available from: &lt;<a href="' + window.location +
+                         '"" class=\"url\">' + window.location +
+                         '</a>&gt;<br/>[Cited ' +
+                         moment().format('Do MMMM, YYYY') + '].');
 
-  /*
-   * RECORD ADDED:
-   */
-  $('#date-added').html('Added ' +
-                        moment($('#date-added').attr('data-date')).fromNow() +
-                        '.');
+    /*
+     * RECORD ADDED:
+     */
+    $('#date-added').html('Added ' +
+                          moment($('#date-added').attr('data-date')).fromNow() +
+                          '.');
 
 });
