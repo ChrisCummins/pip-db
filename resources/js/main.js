629429005b0a3ef526f6e672ec1728f52cf3e5fa
Chris Cummins <chrisc.101@gmail.com>
2014-02-10 23:03:08 +0000

    js: Use advanced search form logic on all searches
    This applies the same default value logic used in the advanced search
    page to the main page and navbar search bars as well.

--- resources/js/main.js
+++ resources/js/main.js
@@ -35,4 +35,63 @@
      */
     var value = $("#q").val();
     $("#q").focus().val('').val(value);
+
+  /*
+   * Return a subset of values where each item is different from the
+   * default. If no default is given for a value, then return it.
+   */
+  var stripDefaultValues = function(values, defaults) {
+    var items = {}, length = 0;
+
+    values.forEach(function(e) {
+      if (e.value !== defaults[e.name]) {
+        items[e.name] = e.value;
+        length++;
+      }
+    });
+
+    return {length: length, values: items};
+  };
+
+  $searchForms = $('form[action="/s"]');
+
+  /*
+   * Append the advanced button value to the form values.
+   */
+  $(':submit', $searchForms).click(function() {
+    if ($(this).attr('value') === 'a') {
+      $(this).closest('form').append($("<input type='hidden'>").attr({
+        name: $(this).attr('name'),
+        value: $(this).attr('value')
+      }));
+    }
+  });
+
+  $searchForms.submit(function(e) {
+    e.preventDefault();
+
+    var items = stripDefaultValues($(this).serializeArray(), {
+      'q': '',
+      'q_eq': '',
+      'q_any': '',
+      'q_ne': '',
+      'q_s': '',
+      'q_l': '',
+      'ec1': '',
+      'ec2': '',
+      'ec3': '',
+      'ec4': '',
+      'pi_l': '',
+      'pi_h': '',
+      'mw_l': '',
+      'mw_h': '',
+      'm': 'Any',
+      't_l': '',
+      't_h': ''
+    });
+
+    if (items.length) // Only submit form if we have some unique values
+      window.location = $(this).attr('action') + '?' + $.param(items.values);
+  });
+
 }());
