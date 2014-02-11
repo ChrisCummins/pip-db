4d1bdae96fb753adc3adf0e6c058a7bf00271ffa
Chris Cummins <chrisc.101@gmail.com>
2014-02-11 18:00:37 +0000

    views/ui: Add a set of widgets

--- src/pip_db/views/ui.clj
+++ src/pip_db/views/ui.clj
@@ -9,6 +9,59 @@
 (defn google-analytics []
   (util/inline-js "/js/google-analytics.inline.js"))
 
+;; ----------
+;; ## Widgets
+;;
+;; A collection of user interface widgets which can be used to
+;; construct pages.
+
+;; ## Input widgets
+;;
+;; These are widgets which are used for user input, such as performing
+;; searches.
+
+;; An input label widget. Optionally, the name of an input can be
+;; provided to assign the label to.
+(defn label-widget
+  ([text]     [:label text])
+  ([for text] [:label {:for for} text]))
+
+;; An info text widget to be used to annotate input forms.
+(defn info-widget [text]
+  [:div.info text])
+
+;; A standard text input widget.
+(defn text-input-widget [name value]
+  [:input {:id name :name name :type "text"
+           :autocomplete "off" :value value}])
+
+;; ### Search form widgets
+;;
+;; A search form is a full-page width entry form for looking up data,
+;; and consists of a number of elements aligned into rows.
+
+(defn search-form-heading-row
+  ([text]    [:div.row [:div.col-md-12 [:h4          text]]])
+  ([id text] [:div.row [:div.col-md-12 [:h4 {:id id} text]]]))
+
+;; A row within a search form consists of three elements, the label,
+;; widget and description.
+(defn search-form-widget-row [label widget description]
+  [:div.row
+   [:div.col-md-2 label]
+   [:div.col-md-6 widget]
+   [:div.col-md-4 description]])
+
+;; A row within a search form for a text search. We can optionally
+;; provide a value to set the text box to.
+(defn search-form-text-row
+  ([name label-text desc-text]
+     (search-form-text-row name label-text desc-text ""))
+  ([name label-text desc-text input-value]
+     (search-form-widget-row (label-widget name (str label-text ":"))
+                             (text-input-widget name input-value)
+                             (info-widget (str desc-text ".")))))
+
 (defn search-bar [data]
   [:form {:method "GET" :action "/s" :role "search"}
    [:div.input-group
