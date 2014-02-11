24fa77108a9fdfebe3c7f44ede10c10211b28883
Chris Cummins <chrisc.101@gmail.com>
2014-02-11 01:20:22 +0000

    util: Add some extra comments

--- src/pip_db/util.clj
+++ src/pip_db/util.clj
@@ -3,12 +3,32 @@
 (ns pip-db.util
   (:use [pip-db.resources :only (resource)]))
 
+;; ## Date utilities
+
 ;; Return the current year as a number, as set by the system clock.
 (defn current-year []
   (+ 1900 (.getYear (new java.util.Date))))
 
+;; ## Working with HTML
+
+;; Returns an element which represents an embedded <style></style>
+;; tag, containing the contents of the given path. Paths use the
+;; `resources/public` directory as their root,
+;; e.g. `/css/foo.inline.css`, which expands to
+;; `resources/public/css/foo.inline.css`. By convention, any file
+;; which is intended to be inlined should have the `.inline` suffix
+;; appended to its name, in order to distinguish it from public CSS
+;; files which are included using the `src` attribute.
 (defn inline-css [path]
   [:style (resource path)])
 
+;; Returns an element which represents an embedded <script></script>
+;; tag, containing the contents of the given path. Paths use the
+;; `resources/public` directory as their root,
+;; e.g. `/js/foo.inline.js`, which expands to
+;; `resources/public/js/foo.inline.js`. By convention, any file which
+;; is intended to be inlined should have the `.inline` suffix appended
+;; to its name, in order to distinguish it from public JS files which
+;; are included using the `src` attribute.
 (defn inline-js [path]
   [:script (resource path)])
