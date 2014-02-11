51e34277babfc3876efd8c555007e24002343c83
Chris Cummins <chrisc.101@gmail.com>
2014-02-11 00:47:32 +0000

    view/index: Refactor into smaller functions

--- src/pip_db/views/index.clj
+++ src/pip_db/views/index.clj
@@ -2,49 +2,57 @@
   (:use [pip-db.views.layout :only (page)]
         [pip-db.views.components :only (inline-js search-bar)]))
 
+;; The "above the fold" content, i.e. the main logo and search bar.
+(defn search-block [data]
+  [:div.search.search-block.text-center
+   [:img {:src "/img/logo-640x226.png"
+          :alt-text "pip-db" :title "pip-db"}]
+   [:div#s (search-bar data)]])
+
+;; The "below the fold" main body of text, containing a description of
+;; what the site is, it's purpose, and how to use it.
+(defn text-body [data]
+  [:div.col-md-12
+   [:p.lead
+    (str "Lorem ipsum dolor sit amet, consectetur "
+         "adipiscing elit. Etiam ut aliquet nisi."
+         "Phasellus sed felis eu ipsum ultricies "
+         "vehicula. Ut sed urna vitae risus dictum "
+         "condimentum vel at lorem.")]
+   [:p.lead
+    (str "Vivamus iaculis congue est, quis rutrum "
+         "mauris tempus quis. Praesent ligula "
+         "libero, lobortis vel mauris et, ornare "
+         "rutrum ligula. Morbi lectus ligula, "
+         "luctus nec vestibulum ac, tincidunt a "
+         "urna. Cras vel lectus at nibh consectetur"
+         " fermentum quis at ligula. Quisque semper "
+         "magna orci, at scelerisque metus "
+         "tincidunt in. Integer orci dui, "
+         "vestibulum nec posuere et, pellentesque "
+         "et elit. Maecenas id ipsum a massa "
+         "tincidunt euismod nec in orci. Duis sit "
+         "amet purus vitae nulla tristique "
+         "fermentum. Vestibulum eu urna ut massa "
+         "sollicitudin aliquam. Sed quis vehicula "
+         "nunc. Praesent lobortis tempus leo eu "
+         "ornare.")]
+   [:p.lead
+    (str "Feugiat vivamus leo et gravida "
+         "vestibulum. Pellentesque placerat, neque "
+         "eu rutrum bibendum, felis justo venenatis "
+         "erat, vel lacinia enim nisl ac metus. Nam "
+         "viverra ut turpis ac euismod. Phasellus "
+         "adipiscing tempus nunc. Mauris congue "
+         "imperdiet sagittis. In rhoncus augue eget "
+         "luctus interdum. Duis tempor nunc sit "
+         "amet aliquet dignissim. Etiam rhoncus "
+         "ante ut leo tristique, eu vehicula erat "
+         "egestas. Nulla facilisi. Maecenas "
+         "volutpat congue velit, vel feugiat quam "
+         "condimentum in.")]])
+
 (defn index [data]
   (page {:navbar {:login-only true}
-         :body (list [:div.row
-                      [:div.search.search-block.text-center
-                       [:img {:src "/img/logo-640x226.png"
-                              :alt-text "pip-db"
-                              :title "pip-db"}]
-
-                       [:div#s (search-bar data)]]]
-                     [:div.row
-                      [:div.col-md-12
-                       [:p.lead (str "Lorem ipsum dolor sit amet, consectetur "
-                                     "adipiscing elit. Etiam ut aliquet nisi."
-                                     "Phasellus sed felis eu ipsum ultricies "
-                                     "vehicula. Ut sed urna vitae risus dictum "
-                                     "condimentum vel at lorem.")]
-                       [:p.lead (str "Vivamus iaculis congue est, quis rutrum "
-                                     "mauris tempus quis. Praesent ligula "
-                                     "libero, lobortis vel mauris et, ornare "
-                                     "rutrum ligula. Morbi lectus ligula, "
-                                     "luctus nec vestibulum ac, tincidunt a "
-                                     "urna. Cras vel lectus at nibh consectetur"
-                                     " fermentum quis at ligula. Quisque semper "
-                                     "magna orci, at scelerisque metus "
-                                     "tincidunt in. Integer orci dui, "
-                                     "vestibulum nec posuere et, pellentesque "
-                                     "et elit. Maecenas id ipsum a massa "
-                                     "tincidunt euismod nec in orci. Duis sit "
-                                     "amet purus vitae nulla tristique "
-                                     "fermentum. Vestibulum eu urna ut massa "
-                                     "sollicitudin aliquam. Sed quis vehicula "
-                                     "nunc. Praesent lobortis tempus leo eu "
-                                     "ornare.")]
-                       [:p.lead (str "Feugiat vivamus leo et gravida "
-                                     "vestibulum. Pellentesque placerat, neque "
-                                     "eu rutrum bibendum, felis justo venenatis "
-                                     "erat, vel lacinia enim nisl ac metus. Nam "
-                                     "viverra ut turpis ac euismod. Phasellus "
-                                     "adipiscing tempus nunc. Mauris congue "
-                                     "imperdiet sagittis. In rhoncus augue eget "
-                                     "luctus interdum. Duis tempor nunc sit "
-                                     "amet aliquet dignissim. Etiam rhoncus "
-                                     "ante ut leo tristique, eu vehicula erat "
-                                     "egestas. Nulla facilisi. Maecenas "
-                                     "volutpat congue velit, vel feugiat quam "
-                                     "condimentum in.")]]])}))
+         :body (list [:div.row (search-block data)]
+                     [:div.row (text-body data)])}))
