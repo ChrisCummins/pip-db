7805ba45a553ff902e2ae093cd9412ab3a7e55dc
Chris Cummins <chrisc.101@gmail.com>
2014-02-11 21:30:27 +0000

    Bump release version for '0.3.3'

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pip-db "0.3.2"
+(defproject pip-db "0.3.3"
   :description "Protein Isoelectric Point Database"
   :url "https://github.com/ChrisCummins/pip-db"
   :license {:name "GNU General Public License v3"
