dee0988a898295c1c0148ef258ed60ddb4c2e4c9
Chris Cummins <chrisc.101@gmail.com>
2014-02-15 14:29:35 +0000

    controllers/record: Show 404 error if no record
    Issue #241.

--- src/pip_db/controllers/record.clj
+++ src/pip_db/controllers/record.clj
@@ -2,13 +2,14 @@
   (:use [compojure.core :only (defroutes GET)])
   (:require [pip-db.models.record :as model]
             [pip-db.views.advanced :as advanced]
-            [pip-db.views.record :as view]))
+            [pip-db.views.record :as view]
+            [pip-db.views.error :as error]))
 
 (defn record [id]
   (let [data (model/record id)]
     (if data
-      (view/record (model/record id))
-      (view/no-record))))
+      (view/record data)
+      (error/status-404))))
 
 (defroutes routes
   (GET "/record/:id" [id] (record id)))
