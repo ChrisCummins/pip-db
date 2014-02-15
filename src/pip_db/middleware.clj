63fd5f2fd550025e960cf9ab1331a7e227eabc1c
Chris Cummins <chrisc.101@gmail.com>
2014-02-15 19:20:25 +0000

    middleware: Add wrap-cookies middleware

--- src/pip_db/middleware.clj
+++ src/pip_db/middleware.clj
@@ -1,6 +1,7 @@
 ;; # Application middleware
 (ns pip-db.middleware
-  (:use [ring.middleware.params :only (wrap-params)]
+  (:use [ring.middleware.cookies :only (wrap-cookies)]
+        [ring.middleware.params :only (wrap-params)]
         [ring.middleware.multipart-params :only (wrap-multipart-params)]
         [compojure.core :only (defroutes)])
   (:require [pip-db.views.error :as error]
@@ -42,5 +43,6 @@
 (def middleware
   (-> routes
       wrap-exception
+      wrap-cookies
       wrap-params
       wrap-multipart-params))
