e787e038818868bb82f73d5163a8df076bfded11
Chris Cummins <chrisc.101@gmail.com>
2014-02-15 14:47:17 +0000

    Merge middleware logic into middleware ns

--- src/pip_db/middleware.clj
+++ src/pip_db/middleware.clj
@@ -1,7 +1,16 @@
 ;; # Application middleware
 (ns pip-db.middleware
-  (:require [pip-db.views.error :as page]
-            [clojure.string :as str]))
+  (:use [ring.middleware.params :only (wrap-params)]
+        [ring.middleware.multipart-params :only (wrap-multipart-params)]
+        [compojure.core :only (defroutes)])
+  (:require [pip-db.views.error :as error]
+            [pip-db.controllers.index :as index]
+            [pip-db.controllers.search :as search]
+            [pip-db.controllers.record :as record]
+            [pip-db.controllers.login :as login]
+            [pip-db.controllers.upload :as upload]
+            [clojure.string :as str]
+            [compojure.route :as route]))
 
 ;; ## Ring handlers
 ;;
@@ -10,6 +19,14 @@
 ;; > process a given request map to generate and return a response
 ;; > map.
 ;;
+(defroutes routes
+  index/routes
+  search/routes
+  record/routes
+  login/routes
+  upload/routes
+  (route/resources "/")
+  (route/not-found (error/status-404)))
 
 ;; In order to handle exceptions which are caused while processing
 ;; requests, we have an exception wrapper which can generate a 500
@@ -19,5 +36,11 @@
     (try (f request)
          (catch Exception e
            (if (str/blank? (System/getenv "DEBUG"))
-             {:status 500 :body (page/status-500)}
-             {:status 500 :body (page/status-500 e)})))))
+             {:status 500 :body (error/status-500)}
+             {:status 500 :body (error/status-500 e)})))))
+
+(def middleware
+  (-> routes
+      wrap-exception
+      wrap-params
+      wrap-multipart-params))
