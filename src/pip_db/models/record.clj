cfacbf6f8c648cde9cc61d172d02660f5e89a3fb
Chris Cummins <chrisc.101@gmail.com>
2014-02-15 14:32:11 +0000

    models/record: Test for correct ID number
    Issue #241.

--- src/pip_db/models/record.clj
+++ src/pip_db/models/record.clj
@@ -1,13 +1,19 @@
 (ns pip-db.models.record
   (:use [pip-db.query :only (AND OR EQ NE)])
   (:require [clojure.java.jdbc :as sql]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [pip-db.util :as util]))
 
 (defn query-string [id]
   (str "SELECT * FROM records WHERE id='" id "'"))
 
-(defn record [id]
+;; Fetch the record data for the given ID.
+(defn record-data [id]
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/with-query-results results
       [(query-string id)]
       (when results (first (doall results))))))
+
+(defn record [id]
+  (if (integer? (util/string->int id))
+    (record-data id)))
