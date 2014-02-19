96623f5b822185ae436532edf78d6b4c89c8bd15
Chris Cummins <chrisc.101@gmail.com>
2014-02-19 17:01:38 +0000

    views: Remove old UI fluff
    We're no longer using:
    
     * Beta warning messages - these are not needed now that the meta
       results shows when results have been limited, e.g. "Found 50 results
       of a possible 5771 and returned the first 20...".
    
     * Pagination - There's no use to it now that we have a properly
       enforced maximum number of results.
    
    Issue #272.

--- resources/js/search.inline.js
+++ resources/js/search.inline.js
@@ -97,22 +97,6 @@
         $noResultsMessage.show();
     }
 
-    /*
-     * PAGINATION:
-     *
-     * Add hyperlinks to pagination links.
-     */
-    var results_per_page = $pagination.attr('data-results-per-page'),
-        url = window.location.toString().replace(/&start=\d+/, ''),
-        get_results_page_url = function (n) {
-            if (n > 1) { return url + '&start=' + (n - 1) * results_per_page; }
-            return url;
-        };
-
-    $('#pagination a.page-ref').each(function (index) {
-        $(this).attr('href', get_results_page_url($(this).attr('data-page')));
-    });
-
     $(document).ready(function () {
         // Set the Download link
         $download.attr('href', (function () {
--- src/pip_db/views/search.clj
+++ src/pip_db/views/search.clj
@@ -14,45 +14,12 @@
             [:td.location "Location"]
             [:td.pi       "pI"]]] [:tbody]])
 
-(defn page-links [current-page pages pages-count]
-  (list
-   (if (> (first pages) 1)
-     [:a.page-ref.btn.btn-success {:data-page 1} "&laquo;"])
-   (for [page pages]
-     [:a.page-ref.btn.btn-success
-      (if (= page current-page)
-        {:data-page page :class "disabled"}
-        {:data-page page}) page])
-   (if (< (last pages) pages-count)
-     [:a.page-ref.btn.btn-success {:data-page pages-count} "&raquo;"])))
-
-;; Generate the row of pagination links, if required. We determine if
-;; we need pagination links based on whether we have more than one
-;; page or not.
-(defn pagination-links [current-page pages results-per-page pages-count]
-  (if (> pages-count 1)
-    [:div.row {:style "margin-bottom: 20px;"}
-     [:div.col-lg-12
-      [:div {:style "text-align: center;"}
-       [:div#pagination.btn-group {:style "margin: 0 auto;"
-                                   :data-results-per-page results-per-page
-                                   :data-pages-count pages-count}
-        (page-links current-page pages pages-count)]]]]))
-
-(def beta-warning
-  [:div#limited-results.alert.alert-info {:style "display:none;"}
-   [:strong "Limited Results "]
-   "The number of results has been limited for the beta version of the website."
-   [:a.close {:href "#" :data-dismiss "alert"
-              :aria-hidden "true"} "&times;"]])
-
 (defn search [request]
   (page request
         {:title ((request :params) "q")
          :navbar {:search true}
          :heading {:meta true :download true}
          :body [:div.sresults
-                beta-warning
                 results-table
                 ui/no-results-found-message]
          :javascript (list (util/inline-data-js "data" (request :results))
