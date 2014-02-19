bbfef93916904750ad6ace36d899de876c504d8c
Chris Cummins <chrisc.101@gmail.com>
2014-02-19 15:22:49 +0000

    search: Generate results table on client side
    This means that the server no longer generates a static HTML table and
    transmits it, but instead embeds a JSON object containing the encoded
    results, which are then assembled at the client's end to populate the
    table.
    
    Issue #272.

--- resources/js/search.inline.js
+++ resources/js/search.inline.js
@@ -1,34 +1,80 @@
-$(document).ready(function () {
+(function () {
     'use strict';
 
     // The base URL component for results
     var resultsUrlPrefix = '/r/'
 
     var $download = $('#download');
-
-    $download.attr('href', (function () {
-        var downloadPageUrl = '/d'
-        var href = window.location.href;
-        var queryString = href.slice(href.indexOf('?') + 1);
-
-        return downloadPageUrl + '?' + queryString;
-    })());
+    var $table = $('.sresults table');
+    var $tbody = $(' tbody', $table);
+    var $pagination = $('#pagination');
 
     /*
      * SEARCH RESULTS:
      *
      * Link each result to its corresponding record page.
      */
-    $('.sresults table tbody tr').click(function () {
-        window.location = resultsUrlPrefix + $(this).attr('data-id');
-    });
+    var attachTableListeners = function () {
+        $(' tr', $tbody).click(function () {
+            window.location = resultsUrlPrefix + $(this).attr('data-id');
+        });
+    };
+
+    // Add a new record entry to the results table
+    var addRecordRow = function (record) {
+
+        // Get the HTML for a table cell
+        var getCell = function (name, value) {
+            return '<td class="' + name + '">' + value + '</td>';
+        };
+
+        // Get the HTML for a table cell from a record property
+        var getRecordCell = function (property) {
+            return getCell(property, record[property]);
+        };
+
+        // Get the HTML for a pI table cell from a record
+        var getPiText = function () {
+            /*
+             * We allow for some flexibility in displaying isoelectric points. We
+             * will try first to show an exact value, else a range of values, or
+             * just an individual result within that range.
+             */
+            var pi      = record['pi'];
+            var piMin   = record['pi_range_min'];
+            var piMax   = record['pi_range_max'];
+            var piMajor = record['pi_major'];
+
+            if (pi)             return pi;
+            if (piMin && piMax) return piMin + ' - ' + piMax;
+            if (piMin)          return '> ' + piMin;
+            if (piMax)          return '< ' + piMax;
+            if (piMajor)        return piMajor + 'm';
+        }
+
+        var html = '<tr data-id="' + record['id'] + '">';
+
+        html += getRecordCell('name');
+        html += getRecordCell('source');
+        html += getRecordCell('organ');
+        html += getCell('pi', getPiText());
+
+        $tbody.append(html + '</tr>');
+    };
+
+    // Populate the table
+    for (var i in data)
+        addRecordRow(data[i]);
+
+    // Add link handlers to record page
+    attachTableListeners();
 
     /*
      * PAGINATION:
      *
      * Add hyperlinks to pagination links.
      */
-    var results_per_page = $('#pagination').attr('data-results-per-page'),
+    var results_per_page = $pagination.attr('data-results-per-page'),
         url = window.location.toString().replace(/&start=\d+/, ''),
         get_results_page_url = function (n) {
             if (n > 1) { return url + '&start=' + (n - 1) * results_per_page; }
@@ -39,4 +85,15 @@ $(document).ready(function () {
         $(this).attr('href', get_results_page_url($(this).attr('data-page')));
     });
 
-});
+    $(document).ready(function () {
+        // Set the Download link
+        $download.attr('href', (function () {
+            var downloadPageUrl = '/d'
+            var href = window.location.href;
+            var queryString = href.slice(href.indexOf('?') + 1);
+
+            return downloadPageUrl + '?' + queryString;
+        })());
+    });
+
+}());
--- src/pip_db/views/search.clj
+++ src/pip_db/views/search.clj
@@ -3,43 +3,14 @@
   (:require [clojure.string :as str]
             [pip-db.util :as util]))
 
-;; If a range of pI values are to be displayed, then we use a
-;; separator string to delimit the minimum and maximum values.
-(def pi-range-separator "-")
-
-;; We allow for some flexibility in displaying isoelectric points. We
-;; will try first to show an exact value, else a range of values, or
-;; just an individual result within that range.
-(defn pi-text [record]
-  (let [pi       (record :pi)
-        pi-min   (record :pi_range_min)
-        pi-max   (record :pi_range_max)
-        pi-major (record :pi_major)]
-
-    (if (not (str/blank? pi))
-      pi
-      (if (not (str/blank? pi-major))
-        (str pi-major "m")
-        (if (and (not (str/blank? pi-min))
-                 (not (str/blank? pi-max)))
-          (str pi-min pi-range-separator pi-max)
-          (str pi-min pi-max))))))
-
-(defn tablify-results [results]
+;; The empty table in which results can be shown
+(def results-table
   [:table.table.table-striped.table-hover.table-bordered
-   [:thead
-    [:tr
-     [:td.name     "Protein"]
-     [:td.source   "Source"]
-     [:td.location "Location"]
-     [:td.pi       "pI"]]]
-   [:tbody
-    (for [record results]
-      [:tr {:data-id (record :id)}
-       [:td.name     (record :name)]
-       [:td.source   (record :source)]
-       [:td.location (record :organ)]
-       [:td.pi       (pi-text record)]])]])
+   [:thead [:tr
+            [:td.name     "Protein"]
+            [:td.source   "Source"]
+            [:td.location "Location"]
+            [:td.pi       "pI"]]] [:tbody]])
 
 (defn page-links [current-page pages pages-count]
   (list
@@ -85,9 +56,10 @@
                   (list
                    (if (request :limited-results)
                      (beta-warning))
-                   (tablify-results (request :results))
+                   results-table
                    (pagination-links (request :current-page) (request :pages)
                                      (request :results-per-page)
                                      (request :pages-count)))
                   [:p.lead "No results found."])]
-         :javascript (util/inline-js "/js/search.inline.js")}))
+         :javascript (list (util/inline-data-js "data" (request :results))
+                           (util/inline-js "/js/search.inline.js"))}))
