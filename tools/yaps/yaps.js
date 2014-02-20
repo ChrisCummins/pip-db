9816ba9445b7f213ce535272b09435b1e6b6651e
Chris Cummins <chrisc.101@gmail.com>
2014-02-20 13:12:37 +0000

    tools/yaps: Add file reading mechanics
    Issue #282.

--- tools/yaps/yaps.js
+++ tools/yaps/yaps.js
@@ -4,4 +4,27 @@
  * yaps.js - Yet Another Protein Schema
  */
 
-console.log('Hello, world!');
+var lazy = require("lazy");
+var fs  = require("fs");
+
+var processLine = function (line) {
+
+};
+
+var argv = process.argv, argc = argv.length;
+
+if (argc !== 3) {
+  console.log('Usage <csv-file>');
+  process.exit(1);
+}
+
+var csv = argv[2];
+var readStream = fs.createReadStream(csv);
+
+readStream.on('error', function (error) {
+  console.log('Unable to read file "' + csv + '"!');
+});
+
+new lazy(readStream)
+  .lines
+  .forEach(processLine);
