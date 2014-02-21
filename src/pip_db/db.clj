8e273491399d67e3b1dae4b472abe4e31d9858f5
Chris Cummins <chrisc.101@gmail.com>
2014-02-21 00:08:41 +0000

    Replace CSV parser with YAPS parser
    Issue #284.

--- src/pip_db/db.clj
+++ src/pip_db/db.clj
@@ -3,7 +3,8 @@
 ;; This namespace defines the interface and API for the database
 ;; back-end of pip-db.
 (ns pip-db.db
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as sql]
+            [pip-db.util :as util]))
 
 ;; SHA1 implementation
 ;;
@@ -31,38 +32,119 @@
 (defn create-tables []
   (sql/with-connection (System/getenv "DATABASE_URL")
     (sql/create-table :records
-                      [:id "varchar(11)" "NOT NULL"]
-                      [:dataset :varchar]
-                      [:ec :varchar]
+                      [:id            "varchar(11)" "NOT NULL"]
+                      [:ec            :varchar]
+                      [:source        :varchar]
+                      [:location      :varchar]
+                      [:mw_min        :varchar]
+                      [:mw_max        :varchar]
+                      [:sub_no        :varchar]
+                      [:sub_mw        :varchar]
+                      [:iso_enzymes   :varchar]
+                      [:pi_min        :varchar]
+                      [:pi_max        :varchar]
+                      [:pi_major      :varchar]
+                      [:temp_min      :varchar]
+                      [:temp_max      :varchar]
+                      [:method        :varchar]
+                      [:ref_full      :varchar]
+                      [:ref_abstract  :varchar]
+                      [:ref_pubmed    :varchar]
+                      [:ref_taxonomy  :varchar]
+                      [:ref_sequence  :varchar]
+                      [:notes         :varchar]
+                      [:real_ec1      :integer]
+                      [:real_ec2      :integer]
+                      [:real_ec3      :integer]
+                      [:real_ec4      :integer]
+                      [:real_mw_min   :real]
+                      [:real_mw_max   :real]
+                      [:real_pi_min   :real]
+                      [:real_pi_max   :real]
+                      [:real_temp_min :real]
+                      [:real_temp_max :real]
+                      [:created_at    :timestamp
+                       "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])
+
+    (sql/create-table :protein_names
                       [:name :varchar]
-                      [:alt_name :varchar]
-                      [:source :varchar]
-                      [:organ :varchar]
-                      [:mw :varchar]
-                      [:sub_no :varchar]
-                      [:sub_mw :varchar]
-                      [:no_iso :varchar]
-                      [:pi_max :varchar]
-                      [:pi_range_min :varchar]
-                      [:pi_range_max :varchar]
-                      [:pi_major :varchar]
-                      [:pi :varchar]
-                      [:temp :varchar]
-                      [:method :varchar]
-                      [:valid :varchar]
-                      [:sequence :varchar]
-                      [:species :varchar]
-                      [:citations :varchar]
-                      [:abstract :varchar]
-                      [:pubmed :varchar]
-                      [:notes :varchar]
-                      [:created_at :timestamp "NOT NULL"
-                       "DEFAULT CURRENT_TIMESTAMP"])
+                      [:records_id "varchar(11)" "NOT NULL"])
 
     (sql/create-table :users
                       [:id :serial "PRIMARY KEY"]
                       [:email :varchar "NOT NULL"]
                       [:pass :varchar "NOT NULL"])))
+
+;; Generate a unique ID for a line.
+(defn record-hash [record]
+  (minihash (str record)))
+
+;; YAPS map
+
+(defn add-record [r]
+  (let [id              (record-hash r)
+        names           (r "names")
+        ec_arr          (if (r "ec") (r "ec") {})
+        ec0             (ec_arr 0)
+        ec1             (ec_arr 1)
+        ec2             (ec_arr 2)
+        ec3             (ec_arr 3)
+        ec              (if ec0 (str ec0 "." ec1 "." ec2 "." ec3))
+        source          (r "source")
+        location        (r "location")
+        mw              (if (r "mw") (r "mw") {})
+        mw_min          (mw "min")
+        mw_max          (mw "max")
+        sub             (if (r "subunit") (r "subunit") {})
+        sub_no          (sub "no")
+        sub_mw          (sub "mw")
+        iso_enzymes     (r "iso_enzymes")
+        pi              (if (r "pi") (r "pi") {})
+        pi_min          (pi "min")
+        pi_max          (pi "max")
+        pi_major        (pi "major")
+        temp            (if (r "temp") (r "temp") {})
+        temp_min        (temp "min")
+        temp_max        (temp "max")
+        method          (r "method")
+        ref             (if (r "references") (r "references") {})
+        ref_orig        (if (ref "original_text") (ref "original_text") {})
+        ref_full        (ref_orig "full")
+        ref_abstract    (ref_orig "abstract")
+        ref_pubmed      (ref "pubmed")
+        ref_taxonomy    (ref "taxonomy")
+        ref_sequence    (ref "sequence")
+        notes           (r "notes")
+        real_ec1        (util/str->int ec0)
+        real_ec2        (util/str->int ec1)
+        real_ec3        (util/str->int ec2)
+        real_ec4        (util/str->int ec3)
+        real_mw_min     (util/str->int mw_min)
+        real_mw_max     (util/str->int mw_max)
+        real_pi_min     (util/str->num pi_min)
+        real_pi_max     (util/str->num pi_max)
+        real_temp_min   (util/str->int temp_min)
+        real_temp_max   (util/str->int temp_max)]
+
+    (sql/with-connection (System/getenv "DATABASE_URL")
+
+      ;; Insert records
+      (sql/insert-values
+       :records
+       [:id :ec :source :location :mw_min :mw_max :sub_no :sub_mw :iso_enzymes
+        :pi_min :pi_max :pi_major :temp_min :temp_max :method :ref_full
+        :ref_abstract :ref_pubmed :ref_taxonomy :ref_sequence :notes :real_ec1
+        :real_ec2 :real_ec3 :real_ec4 :real_mw_min :real_mw_max :real_pi_min
+        :real_pi_max :real_temp_min :real_temp_max]
+       [id ec source location mw_min mw_max sub_no sub_mw iso_enzymes
+        pi_min pi_max pi_major temp_min temp_max method ref_full
+        ref_abstract ref_pubmed ref_taxonomy ref_sequence notes real_ec1
+        real_ec2 real_ec3 real_ec4 real_mw_min real_mw_max real_pi_min
+        real_pi_max real_temp_min real_temp_max])
+
+      ;; Insert names
+      (doseq [name names]
+        (sql/insert-values :protein_names [:name :records_id] [name id])))))
 
 (defn migrate []
   (when-not (migrated?)
--- src/pip_db/models/upload.clj
+++ src/pip_db/models/upload.clj
@@ -4,6 +4,7 @@
         [clojure-csv.core :only (parse-csv)])
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as str]
+            [clojure.data.json :as json]
             [pip-db.db :as db])
   (:import [java.io File]))
 
@@ -12,27 +13,7 @@
     (copy (tempfile :tempfile) (File. file))
     file))
 
-;; Generate a unique ID for a line.
-(defn id [line]
-  (db/minihash (str/join line)))
-
-(defn store-line-record [fields]
-  (sql/with-connection (System/getenv "DATABASE_URL")
-    (sql/insert-values :records
-                       [:id :dataset :ec :name :alt_name :source :organ :mw
-                        :sub_no :sub_mw :no_iso :pi_max :pi_range_min
-                        :pi_range_max :pi_major :pi :temp :method :valid
-                        :sequence :species :citations :abstract :pubmed
-                        :notes]
-                       [(id fields) (fields 0) (fields 1) (fields 2) (fields 3)
-                        (fields 4) (fields 5) (fields 6) (fields 7) (fields 8)
-                        (fields 9) (fields 10) (fields 11) (fields 12)
-                        (fields 13) (fields 14) (fields 15) (fields 16)
-                        (fields 17) (fields 18) (fields 19) (fields 20)
-                        (fields 21) (fields 22) (fields 23)])))
-
-(defn parse-csv-file [file]
-  (doseq [fields (parse-csv (slurp file) :delimiter \tab)]
-    (if (= (count fields) 24)
-      (store-line-record fields)))
+(defn parse-json-file [file]
+  (doseq [line (json/read-str (slurp file))]
+    (db/add-record line))
   "WOO")
