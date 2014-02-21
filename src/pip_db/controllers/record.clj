d431860a1c2d48ccb9351647d84a0cbf5ed8e1fa
Chris Cummins <chrisc.101@gmail.com>
2014-02-21 01:45:17 +0000

    model: Wrap record logic around search
    Issue #278.

--- src/pip_db/controllers/record.clj
+++ src/pip_db/controllers/record.clj
@@ -5,11 +5,8 @@
             [pip-db.views.record :as view]
             [pip-db.views.error :as error]))
 
-(defn do-record [id request]
-  (let [data (model/record id)]
-    (if data
-      (view/record (merge data request))
-      (error/status-404))))
-
 (defn handler [request]
-  (do-record ((request :params) :id) request))
+  (let [data (model/record ((request :params) :id))]
+    (if data
+      (view/record (assoc request :results data))
+      (error/status-404))))
--- src/pip_db/models/record.clj
+++ src/pip_db/models/record.clj
@@ -2,14 +2,9 @@
   (:use [pip-db.query :only (AND OR EQ NE)])
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as str]
+            [pip-db.models.search :as search]
             [pip-db.util :as util]))
-
-(defn query-string [id]
-  (str "SELECT * FROM records WHERE id='" id "'"))
 
 ;; Fetch the record data for the given ID.
 (defn record [id]
-  (sql/with-connection (System/getenv "DATABASE_URL")
-    (sql/with-query-results results
-      [(query-string id)]
-      (when results (first (doall results))))))
+  (search/search {"id" id}))
