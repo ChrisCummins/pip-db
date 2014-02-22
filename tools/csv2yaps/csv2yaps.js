1b034686ad29c029efeda6a5460e5e5a509077c3
Chris Cummins <chrisc.101@gmail.com>
2014-02-22 13:03:18 +0000

    tools/csv2yaps: Fix statement with no side-effects
    Issue #300.

--- tools/csv2yaps/csv2yaps.js
+++ tools/csv2yaps/csv2yaps.js
@@ -114,7 +114,7 @@ var tokens2Row = function (tokens) {
         str = tokens[schemaProp.indexes[j]];
 
         if (str !== undefined)
-          str.trim();
+          str = str.trim();
         else {
           error('Line is too short. No column for property "' +
                 schemaProp.name + '". Results may be corrupted.');
