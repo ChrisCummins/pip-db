f0730e0b4dddd0a81f17d6f5fdd272acb32f3460
Chris Cummins <chrisc.101@gmail.com>
2014-02-27 11:44:27 +0000

    Refactor YAPS logic out of db ns

--- src/pip_db/db.clj
+++ src/pip_db/db.clj
@@ -7,6 +7,7 @@
             [clojure.string :as str]
             [clojure.set :as set]
             [pip-db.query :as query]
+            [pip-db.yaps :as yaps]
             [pip-db.util :as util]))
 
 ;; Our database spec.
@@ -118,64 +119,6 @@
 (def created-record-fields
   (filter #(not (some #{%} derived-record-fields)) (map first (tables :records))))
 
-;; Convert a YAPS encoded record into a vector of values, using the
-;; schema defined in the records table.
-(defn record->vector [r]
-  (let [id              (util/minihash (str r))
-        names           (str/join " / " (r "Protein-Names"))
-        ec              (r "EC")
-        source          (r "Source")
-        location        (r "Location")
-        mw_min          (r "MW-Min")
-        mw_max          (r "MW-Max")
-        sub_no          (r "Subunit-No")
-        sub_mw          (r "Subunit-MW")
-        iso_enzymes     (r "No-Of-Iso-Enzymes")
-        pi_min          (r "pI-Min")
-        pi_max          (r "pI-Max")
-        pi_major        (r "pI-Major-Component")
-        temp_min        (r "Temperature-Min")
-        temp_max        (r "Temperature-Max")
-        method          (r "Method")
-        ref_full        (r "Full-Text")
-        ref_abstract    (r "Abstract-Only")
-        ref_pubmed      (r "PubMed")
-        ref_taxonomy    (r "Species-Taxonomy")
-        ref_sequence    (r "Protein-Sequence")
-        notes           (r "Notes")
-        sequence        (r "Sequence")
-        real_ec         (if ec (str/split ec #"\.") [])
-        real_ec1        (util/str->int (nth real_ec 0 nil))
-        real_ec2        (util/str->int (nth real_ec 1 nil))
-        real_ec3        (util/str->int (nth real_ec 2 nil))
-        real_ec4        (util/str->int (nth real_ec 3 nil))
-        real_mw_min     (util/str->int mw_min)
-        real_mw_max     (util/str->int mw_max)
-        real_pi_min     (util/str->num pi_min)
-        real_pi_max     (util/str->num pi_max)
-        real_temp_min   (util/str->int temp_min)
-        real_temp_max   (util/str->int temp_max)]
-    [id names ec source location mw_min mw_max sub_no sub_mw iso_enzymes pi_min
-     pi_max pi_major temp_min temp_max method ref_full ref_abstract ref_pubmed
-     ref_taxonomy ref_sequence notes sequence real_ec1 real_ec2 real_ec3
-     real_ec4 real_mw_min real_mw_max real_pi_min real_pi_max real_temp_min
-     real_temp_max]))
-
-;; Return a vector of non-nil values for the given property from
-;; within a set of records.
-(defn records->properties [property records]
-  (remove nil? (flatten (map #(get % property) records))))
-
-;; Return a vector of individual words contained within the given
-;; properties of a set of records. Words list is filtered to remove
-;; single or double character words, words with only numbers, words
-;; with unbalanced parenthesis etc.
-(defn records->property-words [property records]
-  (let [properties (records->properties property records)
-        words      (flatten (map #(str/split % #"\s+") properties))]
-    (filter #(not (re-matches #"(\w\w?)|(\d+)|(\([^\)]*)|([^\(]*\))" %))
-            (map (comp str/capitalize str/lower-case) words))))
-
 ;; Accepts a vector of property values and constructs an autocomplete
 ;; frequency table.
 (defn ac-table [properties]
@@ -183,11 +126,11 @@
 
 ;; Add a set of YAPS encoded records to the database.
 (defn add-records [& records]
-  (let [names     (ac-table (records->properties     "Protein-Names" records))
-        words     (ac-table (records->property-words "Protein-Names" records))
-        sources   (ac-table (records->properties     "Source"        records))
-        locations (ac-table (records->properties     "Location"      records))
-        methods   (ac-table (records->properties     "Method"        records))
+  (let [names     (ac-table (yaps/records->properties     "Protein-Names" records))
+        words     (ac-table (yaps/records->property-words "Protein-Names" records))
+        sources   (ac-table (yaps/records->properties     "Source"        records))
+        locations (ac-table (yaps/records->properties     "Location"      records))
+        methods   (ac-table (yaps/records->properties     "Method"        records))
         ac-keys   (map first ac-table-keys)]
     (with-connection
       (if names     (apply sql/insert-values :ac_names     ac-keys names))
@@ -197,7 +140,7 @@
       (if methods   (apply sql/insert-values :ac_methods   ac-keys methods))
 
       (apply sql/insert-values :records
-             (vec created-record-fields) (map record->vector records)))))
+             (vec created-record-fields) (map yaps/record->vector records)))))
 
 ;; The `with-query-results` function returns a response map of field
 ;; names to values, with the field names all lower-cased for some
