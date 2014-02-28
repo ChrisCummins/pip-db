d5a802e833d5fb1836e75b8d1c6491c772b846d2
Chris Cummins <chrisc.101@gmail.com>
2014-02-28 13:24:09 +0000

    pages/index: Add Darren's front page text

--- src/pip_db/pages/index.clj
+++ src/pip_db/pages/index.clj
@@ -3,6 +3,25 @@
 
 ;; ## View
 
+(def body-text
+  [["The isoelectric point or pI of a protein corresponds to the"
+    "solution pH at which the net surface charge is zero. In the"
+    "denatured state, the pI depends solely on a protein’s amino acid"
+    "composition."]
+   ["Since the earliest days of solution biochemistry, the pI has been"
+    "recorded and reported. Literature reports of pI abound."]
+   ["The protein isoelectric point database (PIP-DB) has collected and"
+    "collated this legacy data to provide an increasingly comprehensive"
+    "database for comparison and benchmarking purposes."]
+   ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut"
+    "aliquet nisi.  Phasellus sed felis eu ipsum ultricies vehicula. Ut"
+    "sed urna vitae risus dictum condimentum vel at lorem. Vivamus iaculis "
+    "congue est, quis rutrum mauris tempus quis. Praesent ligula libero, "
+    "lobortis vel mauris et, ornare rutrum ligula. Morbi lectus ligula, "
+    "luctus nec vestibulum ac, tincidunt a urna. Cras vel lectus at nibh "
+    "consectetur fermentum quis at ligula. Quisque semper magna orci, at "
+    "scelerisque metus tincidunt in."]])
+
 ;; The "above the fold" content, i.e. the main logo and search bar.
 (defn search-block [request]
   [:div.search.search-block.text-center
@@ -11,45 +30,7 @@
 ;; The "below the fold" main body of text, containing a description of
 ;; what the site is, it's purpose, and how to use it.
 (defn text-body [request]
-  [:div.col-md-12
-   [:p.lead
-    (str "Lorem ipsum dolor sit amet, consectetur "
-         "adipiscing elit. Etiam ut aliquet nisi."
-         "Phasellus sed felis eu ipsum ultricies "
-         "vehicula. Ut sed urna vitae risus dictum "
-         "condimentum vel at lorem.")]
-   [:p.lead
-    (str "Vivamus iaculis congue est, quis rutrum "
-         "mauris tempus quis. Praesent ligula "
-         "libero, lobortis vel mauris et, ornare "
-         "rutrum ligula. Morbi lectus ligula, "
-         "luctus nec vestibulum ac, tincidunt a "
-         "urna. Cras vel lectus at nibh consectetur"
-         " fermentum quis at ligula. Quisque semper "
-         "magna orci, at scelerisque metus "
-         "tincidunt in. Integer orci dui, "
-         "vestibulum nec posuere et, pellentesque "
-         "et elit. Maecenas id ipsum a massa "
-         "tincidunt euismod nec in orci. Duis sit "
-         "amet purus vitae nulla tristique "
-         "fermentum. Vestibulum eu urna ut massa "
-         "sollicitudin aliquam. Sed quis vehicula "
-         "nunc. Praesent lobortis tempus leo eu "
-         "ornare.")]
-   [:p.lead
-    (str "Feugiat vivamus leo et gravida "
-         "vestibulum. Pellentesque placerat, neque "
-         "eu rutrum bibendum, felis justo venenatis "
-         "erat, vel lacinia enim nisl ac metus. Nam "
-         "viverra ut turpis ac euismod. Phasellus "
-         "adipiscing tempus nunc. Mauris congue "
-         "imperdiet sagittis. In rhoncus augue eget "
-         "luctus interdum. Duis tempor nunc sit "
-         "amet aliquet dignissim. Etiam rhoncus "
-         "ante ut leo tristique, eu vehicula erat "
-         "egestas. Nulla facilisi. Maecenas "
-         "volutpat congue velit, vel feugiat quam "
-         "condimentum in.")]])
+  [:div.col-md-12 (map #(vector :p.lead (apply str %)) body-text)])
 
 (defn view [request]
   (ui/page request
