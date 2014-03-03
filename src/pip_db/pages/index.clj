92a86b310c253b60dfb1d17bc20916c13ec58e42
Chris Cummins <chrisc.101@gmail.com>
2014-03-03 19:50:57 +0000

    pages/index: Fix up page text

--- src/pip_db/pages/index.clj
+++ src/pip_db/pages/index.clj
@@ -4,23 +4,15 @@
 ;; ## View
 
 (def body-text
-  [["The isoelectric point or pI of a protein corresponds to the"
-    "solution pH at which the net surface charge is zero. In the"
-    "denatured state, the pI depends solely on a protein’s amino acid"
+  [["The isoelectric point or pI of a protein corresponds to the "
+    "solution pH at which the net surface charge is zero. In the "
+    "denatured state, the pI depends solely on a protein’s amino acid "
     "composition."]
-   ["Since the earliest days of solution biochemistry, the pI has been"
+   ["Since the earliest days of solution biochemistry, the pI has been "
     "recorded and reported. Literature reports of pI abound."]
-   ["The protein isoelectric point database (PIP-DB) has collected and"
-    "collated this legacy data to provide an increasingly comprehensive"
-    "database for comparison and benchmarking purposes."]
-   ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut"
-    "aliquet nisi.  Phasellus sed felis eu ipsum ultricies vehicula. Ut"
-    "sed urna vitae risus dictum condimentum vel at lorem. Vivamus iaculis "
-    "congue est, quis rutrum mauris tempus quis. Praesent ligula libero, "
-    "lobortis vel mauris et, ornare rutrum ligula. Morbi lectus ligula, "
-    "luctus nec vestibulum ac, tincidunt a urna. Cras vel lectus at nibh "
-    "consectetur fermentum quis at ligula. Quisque semper magna orci, at "
-    "scelerisque metus tincidunt in."]])
+   ["The protein isoelectric point database (PIP-DB) has collected and "
+    "collated this legacy data to provide an increasingly comprehensive "
+    "database for comparison and benchmarking purposes."]])
 
 ;; The "above the fold" content, i.e. the main logo and search bar.
 (defn search-block [request]
