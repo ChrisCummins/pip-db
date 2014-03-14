3c7aedb8283a6ab6d1772c28fd31e52564f410a0
Chris Cummins <chrisc.101@gmail.com>
2014-03-14 13:29:06 +0000

    download: Strip out XML format features
    Supporting this alternative file format is extra work, and it provides
    no practical benefit over the already tested and existing JSON encoding.

--- resources/js/download.inline.js
+++ resources/js/download.inline.js
@@ -1,60 +1,6 @@
 (function () {
     'use strict';
 
-    /*  This work is licensed under Creative Commons GNU LGPL License.
-
-        License: http://creativecommons.org/licenses/LGPL/2.1/
-        Version: 0.9
-        Author:  Stefan Goessner/2006
-        Web:     http://goessner.net/
-    */
-    function json2xml(o) {
-        var toXml = function(v, name, ind) {
-            // Escape a string
-            var escape = function (text) {
-                return String(text).replace(/&/g, '&amp;');
-            };
-
-            var xml = "";
-            if (v instanceof Array) {
-                for (var i = 0, n = v.length; i < n; i++)
-                    xml += ind + toXml(v[i], name, ind + "\t") + '\n';
-            }
-            else if (typeof(v) == "object") {
-                var hasChild = false;
-                xml += ind + "<";
-                xml += Number(name) > -1 ? 'record' : name; // Default
-                for (var m in v) {
-                    if (m.charAt(0) == "@")
-                        xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
-                    else
-                        hasChild = true;
-                }
-                xml += hasChild ? ">\n" : "/>";
-                if (hasChild) {
-                    for (var m in v) {
-                        if (m == "#text")
-                            xml += v[m];
-                        else if (m == "#cdata")
-                            xml += "<![CDATA[" + v[m] + "]]>";
-                        else if (m.charAt(0) != "@")
-                            xml += toXml(v[m], m, ind + "\t") + '\n';
-                    }
-                    xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</";
-                    xml += Number(name) > -1 ? 'record' : name; // Default
-                    xml += ">";
-                }
-            }
-            else {
-                xml += ind + "<" + name + ">" + escape(v) +  "</" + name + ">";
-            }
-            return xml;
-        }, xml="";
-        for (var m in o)
-            xml += toXml(o[m], m, "") + '\n';
-        return xml;
-    }
-
     /*
      * Convert a JSON object into a CSV encoded string.
      */
@@ -183,12 +129,6 @@
             blob: JSON.stringify(records, null, '\t') + '\n',
             mime: 'application/json',
             extension: '.json',
-            select: showText
-        },
-        'XML': {
-            blob: json2xml(records),
-            mime: 'application/xml',
-            extension: '.xml',
             select: showText
         }
     };
