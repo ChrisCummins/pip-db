4a63cbc21fde5aed8e4facf2c5dcf6312514739f
Chris Cummins <chrisc.101@gmail.com>
2014-04-24 03:13:32 +0100

    Add GPL v3 license headers to sources
    Issue #350.

--- scripts/launch-db.sh
+++ scripts/launch-db.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 #
 # Launch a local user instance of the PostgreSQL database.
-
+#
+# Copyright 2014 Chris Cummins.
+#
+# This file is part of pip-db.
+#
+# pip-db is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pip-db is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pip-db.  If not, see <http://www.gnu.org/licenses/>.
+#
 sudo mkdir -pv /var/run/postgresql
 sudo chown "$(whoami)":"$(whoami)" -R /var/run/postgresql
 
