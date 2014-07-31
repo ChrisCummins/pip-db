5b4efac26a03b3caf35c6e7abe7b6fee9e33ba39
Chris Cummins <chrisc.101@gmail.com>
2014-07-31 14:48:50 +0100

    pip-db/search: Add starting no param to search
    This is to enable pagination of results.

--- src/pip_db/search.clj
+++ src/pip_db/search.clj
@@ -2,6 +2,7 @@
 (ns pip-db.search
   (:require [clojure.string :as str]
             [pip-db.blast :as blast]
+            [pip-db.util :as util]
             [pip-db.db :as db]))
 
 (def max-no-of-returned-records 150)
@@ -15,17 +16,21 @@
         blast?                 (not (str/blank? sequence))
         params                 (request :params)
         headers                (request :headers)
+        start?                 (and (contains? params "start")
+                                    (util/is-number? (params "start")))
+        start                  (if start? (util/str->int (params "start")) 0)
         include-query-terms?   (not (= (headers "x-pip-db-query-terms") "None"))
         include-records?       (not (= (headers "x-pip-db-records")     "None"))
         matching-records       (if blast? (blast/search request) (db/search request))
         no-of-records-searched (if blast? (blast/no-of-records)  (db/no-of-records))]
     (merge
      (if include-records?
-       (let [returned-records (take max-no-of-returned-records matching-records)]
-         {:No-Of-Records-Returned  (count returned-records)
-          :Records                 returned-records}))
-     {:No-Of-Records-Matched       (count matching-records)
-      :Max-No-of-Returned-Records  max-no-of-returned-records
-      :No-Of-Records-Searched      no-of-records-searched}
+       (let [returned-records (take max-no-of-returned-records (drop start matching-records))]
+          {:Returned-Records-Starting-At start
+           :No-Of-Records-Returned       (count returned-records)
+           :Records                      returned-records}))
+     {:No-Of-Records-Matched             (count matching-records)
+      :Max-No-of-Returned-Records        max-no-of-returned-records
+      :No-Of-Records-Searched            no-of-records-searched}
      (if include-query-terms?
-       {:Query-Terms               (dissoc params "seq_name")}))))
+       {:Query-Terms                     (dissoc params "seq_name")}))))
